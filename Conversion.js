5ee0167417a4685f1ef1b84879fd1c5a58362a02
Devraj choudhary <112477863+DevrajChdhary@users.noreply.github.com>
2023-12-31 22:40:17 +0530

    Conversions

---
+++ Conversion.js
@@ -0,0 +1,14 @@
+let string="33";
+console.log(typeof string);
+let convert=Number(string);//convert string value to number
+console.log(typeof convert);
+
+let bool=1;
+console.log(typeof bool);
+let convert1=Boolean(bool);
+console.log(typeof convert1);//convert into bool value
+
+let Num=1023;
+console.log(typeof Num);
+let convert2=String(Num);
+console.log(typeof convert2);//Convert int value into  String
