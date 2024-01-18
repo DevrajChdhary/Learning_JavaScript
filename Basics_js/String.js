97f1d85502565083056782c0ef7bfa45624082a1
Devraj choudhary <112477863+DevrajChdhary@users.noreply.github.com>
2024-01-18 13:39:04 +0530

    String

---
+++ Basics_js/String.js
@@ -0,0 +1,19 @@
+const name = "Devraj"
+const repoCount = 30
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Dev-com')
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   Devraj    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
