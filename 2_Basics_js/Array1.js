7f59938328f83ce30209e8f0aef0770648a04df3
Devraj choudhary <112477863+DevrajChdhary@users.noreply.github.com>
2024-01-20 16:52:14 +0530

    Array

---
+++ 2_Basics_js/Array1.js
@@ -0,0 +1,39 @@
+// array
+
+const myArr1 = [0, 1, 2, 3, 4, 5]
+const myArr2 = ["Dev", "Choudhary"]
+
+const myArr3 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(3)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(1));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr1);
+
+const myn1 = myArr1.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr1);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr1);
+console.log(myn2);
