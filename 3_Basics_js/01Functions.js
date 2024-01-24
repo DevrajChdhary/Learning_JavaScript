81d1b5d2866b0903aed1b0ca6c68b49850351dc6
Devraj choudhary <112477863+DevrajChdhary@users.noreply.github.com>
2024-01-24 09:05:40 +0000

    Different Concept

--- 3_Basics_js/01Functions.js
+++ 3_Basics_js/01Functions.js
@@ -17,5 +17,34 @@ function loginUserMessage(username = "sam"){
     }
     return `${username} just logged in`
 }
-const user=loginUserMessage("Devraj");
+const user1=loginUserMessage("Devraj");
 console.log(user);
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Devraj Choudhary",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const MyArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(MyArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
